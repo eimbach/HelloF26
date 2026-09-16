79e4f66e57bc36c52849532c0d51cbc33dbbe462
Alan Bach <bachal@sheridancollege.ca>
2026-09-16 10:33:41 -0400

    First Commit

---
+++ src/hello/Hello.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello;
+import java.util.Scanner;
+
+/**
+ *
+ * @author bacha
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string.");
+        String word = input.nextLine();
+        char[] letters = new char[word.length()];
+        for (int i = 0; i<=word.length()-1; i++){
+            letters[i] = word.charAt(i);
+        }
+        for (int i = word.length()-1; i>=0; i--){
+            System.out.print(letters[i]);
+        }
+        
+        System.out.println(word);
+        
+        
+    }
+    
+}
---
+++ src/hello/StudentList.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello;
+
+/**
+ *
+ * @author bacha
+ */
+public class StudentList {
+    public static void main(String[] args) {
+        Student[] studentList = new Student[3];
+        Student s1 = new Student("1", "Alan");
+        studentList[0] = s1;
+        studentList[1] = new Student("2", "Alice");
+        studentList[2] = new Student("3", "Bob");
+        for (int i = 0; i < studentList.length; i++){
+            System.out.println(studentList[i].getsName());
+        }
+        
+    }
+}

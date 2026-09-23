183b2cfbb70cfcf29086f5a94fc444cc713fcc86
Alan Bach <bachal@sheridancollege.ca>
2026-09-23 10:42:49 -0400

    Added a class "full time student"

---
+++ src/hello/FullTimeStudent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello;
+
+/**
+ *
+ * @author bacha
+ */
+public class FullTimeStudent extends Student {
+
+    public FullTimeStudent(String sID, String sName) {
+        super(sID, sName);
+    }
+    
+}
